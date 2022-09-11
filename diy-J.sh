009a9b7430580f7872eeebe9a845d5c68e31709c
Sturiss <106640910+Sturiss@users.noreply.github.com>
2022-09-11 15:31:35 +0800

    Update diy-J.sh

--- diy-J.sh
+++ diy-J.sh
@@ -6,7 +6,7 @@ num=$(find $CURRENT_DIR -name gradlew  | awk -F"/" '{print NF-1}')
 DIR=$(find $CURRENT_DIR -name gradlew  | cut -d \/ -f$num)
 cd $CURRENT_DIR/$DIR
 #xwalk修复
-sed -i 's/download.01.org\/crosswalk\/releases\/crosswalk\/android\/maven2/raw.githubusercontent.com\/sturiss\/TVBoxDIY\/main/g' $CURRENT_DIR/$DIR/build.gradle
+sed -i 's/download.01.org\/crosswalk\/releases\/crosswalk\/android\/maven2/raw.githubusercontent.com\/sturiss\/TVBoxOS/tree/0907unpdateConfirm\/main/g' $CURRENT_DIR/$DIR/build.gradle
 #名称修改
 sed -i 's/TVBox/追剧神器/g' $CURRENT_DIR/$DIR/app/src/main/res/values/strings.xml
 #背景修改
